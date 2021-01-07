5c5a7dfb7c5a9757c9e5cec94f4d0c07e2daa127
kaushiki <kaushiki.ak88@gmail.com>
2021-01-08 02:35:43 +0530

    first commit

---
+++ array3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+echo "Enter a number: "
+read num
+primefact() {
+while [ $(($num%2)) -eq 0 ]
+do
+	num=$(( $num/2 ))
+	#echo "$num"
+done
+for((i=3; i<=$(($num*2)); i=$i+2))
+do
+	while [ $(($num%$i)) -eq 0 ]
+	do
+		num=$(( $num/i ))
+		#echo "$num"
+	done
+	if [ $num -gt 2 ]
+	then
+		echo "$num"
+	exit
+	fi
+done
+}
+primefact
---
+++ array5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+for((i=1; i<=100; i++))
+do
+	if [ $(($i%11)) -eq 0 ]
+	then
+		echo "$i"
+	fi
+done
